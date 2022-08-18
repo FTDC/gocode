d2ae468bd90f330797be67f63f25558b13596da6
eric <mrtop@live.cn>
2022-08-18 21:04:34 +0800

    切片遍历

--- atguigu.com/chapter04/demo09/main.go
+++ atguigu.com/chapter04/demo09/main.go
@@ -1,17 +1,24 @@
 package main
+
 import (
-	"fmt" 
+	"fmt"
 )
+
 func main() {
 
 	//位运算的演示
-	fmt.Println(2&3) // 2
-	fmt.Println(2|3) // 3
-	fmt.Println(2^3) // 3
-	fmt.Println(-2^2) //-4
+	fmt.Println(2 & 3)  // 2
+	fmt.Println(2 | 3)  // 3
+	fmt.Println(2 ^ 3)  // 3
+	fmt.Println(-2 ^ 2) //-4
 
 	a := 1 >> 2 //0
 	c := 1 << 2 //4
 	fmt.Println("a=", a, "c=", c)
-	
+
+	//  -1 原码 1000 0010  反码： 1111 11101 补码  1111 1110
+	d := -2 >> 2 //
+
+	fmt.Println("nem", d)
+
 }
